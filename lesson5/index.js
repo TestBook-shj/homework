392ee630a88f32909dfc2fb6b8825beb72043767
TestBook-shj <704426676@qq.com>
2019-10-18 18:29:44 +0800

    修改文件名

--- lesson5/index.js
+++ lesson5/index.js
@@ -6,12 +6,12 @@ const cors = require('@koa/cors')
 const app = new Koa()
 const router = new Router()
 
-const middleware = require('./middleware')
+const checkRole = require('./checkRole')
 const Result = require('./result')
 
 router.prefix('/api')
 
-router.post('/user', middleware, async ctx => {
+router.post('/user', checkRole, async ctx => {
   const { name, email } = ctx.request.body
   if (!name || !email) {
     ctx.body = new Result(404, 'name与email不能为空')
